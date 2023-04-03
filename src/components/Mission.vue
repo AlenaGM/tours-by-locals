b9f62873db700f3b52438b0a9ec3c4dca80510d6
AlenaGM <alena.guillaume4@gmail.com>
2023-04-03 10:52:18 +0200

    finished mission section

--- src/components/Mission.vue
+++ src/components/Mission.vue
@@ -5,40 +5,118 @@
     <h3 class="mission__title">
       <span>Главная цель компании —</span> стать сервисом №1 в сфере путешествий
     </h3>
+    <div class="mission__content">
+      <p>
+        Хотим, чтобы Трипстер стал синонимом увлекательного и уникального опыта
+        познания мира. Сегодня в нашей команде сотрудники с опытом работы в
+        топовых компаниях: Яндекс, VK, Ozon, Циан, X5, Авито, Tutu.
+      </p>
+      <p>
+        Их экспертиза помогает совершенствовать сервис и обмениваться друг с
+        другом опытом внутри компании. Мы ищем людей, которые хотят делать
+        классный продукт, делиться своими знаниями и развиваться.
+      </p>
+    </div>
+    <div class="mission__advantage">
+      <p>
+        Трипстер — это прибыльный бизнес, который полностью окупает себя, а
+        значит, не зависит от сторонних инвесторов. Заработанные деньги
+        инвестируются в дальнейший рост, так что даже в условиях нестабильной
+        экономики мы развиваемся.
+      </p>
+    </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
 .mission {
+  display: grid;
+  grid-template-areas:
+    "title title"
+    "content advantage";
+  grid-template-columns: 50% 1fr;
+  justify-items: start;
+  padding: 0 1.5rem;
+  background: linear-gradient(
+      180deg,
+      #ffffff 27.16%,
+      rgba(255, 255, 255, 0) 56.09%
+    ),
+    url(../assets/images/bg_image_3.png) bottom/100% auto no-repeat;
+  border-radius: 0px 0px 80px 80px;
+  @media screen and (min-width: 1345px) {
+    padding: 0 3.75rem;
+  }
+  @media screen and (max-width: 767px) {
+    display: grid;
+    grid-template-areas:
+      "title"
+      "content"
+      "advantage";
+    grid-template-columns: 1fr;
+    padding: 0 1rem;
+    background: linear-gradient(
+        180deg,
+        #ffffff 0%,
+        rgba(255, 255, 255, 0) 56.09%
+      ),
+      url(../assets/images/bg_image_3_mobile.png) bottom/100% auto no-repeat;
+    border-radius: 0px 0px 24px 24px;
+  }
   &__title {
-    font-family: "MuseoSansCyrl-700", sans-serif;
-    font-size: 56px;
-    font-weight: 700;
-    line-height: 64px;
-    text-align: left;
-    color: var(--color-black);
+    grid-area: title;
     margin-bottom: 4rem;
-    span {
-      font-family: "MuseoSansCyrl-700", sans-serif;
-      font-size: 64px;
-      font-weight: 700;
-      line-height: 72px;
-      text-align: left;
-      color: var(--color-black30);
-    }
-    @media screen and (min-width: 1344px) {
-      font-size: 64px;
-      line-height: 72px;
+    width: 740px;
+    @media screen and (min-width: 1345px) {
+      width: 864px;
+      margin-bottom: 4rem;
     }
     @media screen and (max-width: 1024px) {
-      font-size: 48px;
-      line-height: 56px;
+      width: 660px;
       margin-bottom: 3rem;
     }
-    @media screen and (max-width: 500px) {
-      font-size: 32px;
-      line-height: 40px;
+    @media screen and (max-width: 767px) {
+      width: 100%;
+      max-width: 500px;
       margin-bottom: 1rem;
+    }
+  }
+  &__content {
+    grid-area: content;
+    margin-bottom: 580px;
+    @media screen and (max-width: 1024px) {
+      margin-bottom: 480px;
+    }
+    @media screen and (max-width: 767px) {
+      margin-bottom: 24px;
+    }
+  }
+  &__advantage {
+    grid-area: advantage;
+    width: 61%;
+    margin-left: auto;
+    p {
+      font-size: 17px;
+      font-weight: 400;
+      line-height: 24px;
+      opacity: 0.5;
+    }
+    @media screen and (min-width: 1345px) {
+      width: 288px;
+      margin: 0 auto;
+    }
+    @media screen and (max-width: 1024px) {
+      width: 75%;
+    }
+    @media screen and (max-width: 767px) {
+      width: 100%;
+      margin-bottom: 540px;
+    }
+    @media screen and (max-width: 700px) {
+      margin-bottom: 500px;
+    }
+    @media screen and (max-width: 600px) {
+      margin-bottom: 460px;
     }
   }
 }
