6ecb7b109f2d667295f8fd8284021de4ef01a842
AlenaGM <alena.guillaume4@gmail.com>
2023-04-05 12:02:54 +0200

    footer section done

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,46 @@
 <script setup></script>
 
 <template>
-  <footer class="footer"><h2>Присоединяйся</h2></footer>
+  <footer class="footer">
+    <a href="#" target="_blank" rel="noopener noreferrer">Присоединяйся ></a>
+    <div>© 2013–2023, Tripster Limited</div>
+  </footer>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.footer {
+  padding: 0 1.5rem 4rem;
+  display: grid;
+  align-content: space-between;
+  justify-items: center;
+  @media screen and (min-width: 1345px) {
+    padding: 0 3.75rem 4rem;
+  }
+  @media screen and (max-width: 767px) {
+    padding: 0 1rem 1.5rem;
+  }
+  a {
+    font-family: "MuseoSansCyrl", sans-serif;
+    font-size: 80px;
+    font-weight: 700;
+    line-height: 88px;
+    margin-bottom: 184px;
+    &:hover {
+      color: #d42764;
+    }
+    @media screen and (max-width: 767px) {
+      font-size: 32px;
+      line-height: 40px;
+      margin-bottom: 80px;
+    }
+  }
+  div {
+    font-family: "MuseoSansCyrl", sans-serif;
+    font-size: 17px;
+    font-weight: 400;
+    line-height: 24px;
+    color: var(--color-black);
+    opacity: 50%;
+  }
+}
+</style>
